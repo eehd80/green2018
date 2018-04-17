fda62930331255316aaf276982a939fe9720e720
aldo <eehd80@naver.com>
2018-04-17 13:51:23 +0900

    04/16

---
+++ 180411_kakao/script/comm.js
@@ -0,0 +1,12 @@
+$(function () {
+    //menu
+    $('.menu_depth1>li').mouseover(function () {
+        $(this).children('.menu_depth2').stop().slideDown();
+    });
+    $('.menu_depth1>li').mouseout(function(){
+        $(this).children('.menu_depth2').stop().slideUp();
+    });
+    
+    //
+    
+});
